5929ea947916a8227466077ee3514fd4a3548748
Ryan Wenger <ryanwenger@ucsb.edu>
2023-05-23 00:42:25 -0700

    Small fixup to ramdisk creation script

--- ramdisk.sh
+++ ramdisk.sh
@@ -12,12 +12,12 @@ if [ "$(uname)" = "Linux" ]; then
   sudo mount -t ramfs -o size=1g ramfs "$MOUNT_DIR"  # size parameter seems to be ignored here?
   sudo chown -R "$(whoami)":"$(whoami)" "$MOUNT_DIR"
   mount | grep ram
-  echo "To unmount and free memory run \`sudo umount '$MOUNT_DIR'\`"
+  echo "To unmount and free memory run \`sudo umount $(basename "$MOUNT_DIR")\`"
 else
   SIZE=$((1024 * 1024 * 1024))
   DEVICE=$(hdiutil attach -nomount ram://$(($SIZE / 512)) | xargs)
   newfs_hfs -v cs171-ramdisk "$DEVICE"
   diskutil mount -mountPoint "$MOUNT_DIR" "$DEVICE"
   mount | grep ram
-  echo "To unmount and free memory run \`diskutil eject '$MOUNT_DIR'\` or \`hdiutil detach '$DEVICE'\`"
+  echo "To unmount and free memory run \`diskutil eject $(basename "$MOUNT_DIR")\` or \`hdiutil detach '$DEVICE'\`"
 fi
